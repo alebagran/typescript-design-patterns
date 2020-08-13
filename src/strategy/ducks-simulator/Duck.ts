2a1f90f387915e0c76eff85dd2a1ae5577632f23
Marcus Balbi <balbimarcus@gmail.com>
2020-08-12 21:10:45 -0300

    Behaviors

--- src/strategy/ducks-simulator/Duck.ts
+++ src/strategy/ducks-simulator/Duck.ts
@@ -1,3 +1,17 @@
+import FlyBehavior from "./behaviors/fly-behaviors/Flybehavior";
+import QuackBehavior from "./behaviors/quack-behaviors/QuackBehavior";
+
 export default abstract class Duck {
+  private flyBehavior: FlyBehavior;
+  private quackBehavior: QuackBehavior;
+
   abstract display(): void;
+
+  performFly(): void {
+    this.flyBehavior.fly();
+  }
+
+  performQuack(): void {
+    this.quackBehavior.quack();
+  }
 }
---
+++ src/strategy/ducks-simulator/behaviors/quack-behaviors/QuackBehavior.ts
@@ -0,0 +1,3 @@
+export default interface QuackBehavior {
+  quack(): void;
+}
