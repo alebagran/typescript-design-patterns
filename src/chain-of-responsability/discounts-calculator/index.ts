935bba7f3acc921c708177e50c3fe2107244759d
Marcus Balbi <balbimarcus@gmail.com>
2020-08-24 21:38:11 -0300

    testing classes

---
+++ src/chain-of-responsability/discounts-calculator/index.ts
@@ -0,0 +1,10 @@
+import Budged from "./Budget";
+
+const budget = new Budged();
+
+budget
+  .add({ code: "A1", name: "Celular Samsung", price: 1200.0 })
+  .add({ code: "B335", name: "Monitor LG Ultrawide 29", price: 1500.0 })
+  .add({ code: "V887", name: "Suporte Monitor Top", price: 300.0 });
+
+  
